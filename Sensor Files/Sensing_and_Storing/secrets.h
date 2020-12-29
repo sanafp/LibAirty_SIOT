4e792fdfe980ed6109dc2e23e85aab9760b167bc
Sana Pirmohamed <sana.pirmohamed@gmail.com>
2020-12-29 21:11:28 +0400

    Deleted the file from the git repository

--- Sensor Files/Sensing_and_Storing/secrets.h
+++
@@ -1,7 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "####"		//WiFi network name
-#define SECRET_PASS "####"	//WiFi password
-
-#define SECRET_WRITE_APIKEY "#####"   //'Raw Data' ThingSpeak channel write API Key
